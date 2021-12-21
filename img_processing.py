ef4921ed592d6c86a0d8e50b34364a8586d7f009
Konstantin Yarovoy <xyarovoy@fi.muni.cz>
2021-12-21 12:53:10 +0100

    Fixing img_processing to support 2D images

--- img_processing.py
+++ img_processing.py
@@ -1,6 +1,5 @@
 import numpy
 from tifffile import imread
-import imagecodecs
 from typing import Tuple
 
 from numpy import ndarray
@@ -8,19 +7,22 @@ from numpy import ndarray
 Coords = Tuple[int, int, int]
 ImageSize = Tuple[int, int, int]
 
+
 class Cell:
-    def __init__(self, label, volume, surface, centroid: Coords):
+    def __init__(self, label, volume, surface, centroid: Coords, is3d):
         self.label = label
         self.volume = volume
         self.surface = surface
         self.centroid = centroid
-        #https://www.sciencedirect.com/science/article/pii/S1877750318304757
-        # FIXME: this is most likely only for 3D...
-        # FIXME: either calculate outside, or pass 2D/3D flag inside here
-        self.sphericity = numpy.cbrt(36 * numpy.pi * (volume ** 2)) / surface
+        self.is3d = is3d
+        if is3d:
+            # https://www.sciencedirect.com/science/article/pii/S1877750318304757
+            self.roundness = numpy.cbrt(36 * numpy.pi * (volume ** 2)) / surface
+        else:
+            self.roundness = (4 * numpy.pi * volume) / (surface ** 2)
 
     def __str__(self):
-        return f"Cell label: {self.label}, volume: {self.volume}, border pixels: {self.surface}, centroid: {self.centroid}, sphericity: {self.sphericity}"
+        return f"Cell label: {self.label}, volume: {self.volume}, border pixels: {self.surface}, centroid: {self.centroid}, roundness: {self.roundness}"
 
 
 def main():
@@ -30,10 +32,12 @@ def main():
     cells = {}
 
     image = imread('./data/masks_3D.tif')
-    # FIXME: wrap 'image' with a 3D version with 3rd dim of len() = 1
+    is3d = type(image[0][0]) == numpy.ndarray
+    if not is3d:
+        image = [image]
     size = len(image[0][0]), len(image[0]), len(image)
     for z in range(len(image)):
-        print(f"processing layer {z}")
+        print(f"processing layer {z}/{size[2] - 1}")
         for y in range(len(image[0])):
             for x in range(len(image[0][0])):
                 value = image[z][y][x]
@@ -50,7 +54,7 @@ def main():
         obj_volume = volume[label]
         coords = cumul_coords[label]
         centroid = round(coords[0] / obj_volume), round(coords[1] / obj_volume), round(coords[2] / obj_volume)
-        cells[label] = Cell(label, obj_volume, surface[label], centroid)
+        cells[label] = Cell(label, obj_volume, surface[label], centroid, is3d)
         print(cells[label])
 
 
@@ -63,16 +67,16 @@ def is_pixel_at_cell_border(image: ndarray, pixel: Coords, val: int, size: Image
     x, y, z = pixel
 
     # On the border of image:
-    # FIXME: will be always true for 2D images...
-    # FIXME: create two main branches here, one for 2D an for 3D
-    if x == 0 or x == max_x or y == 0 or y == max_y or z == 0 or z == max_z:
+    if x == 0 or x == max_x or y == 0 or y == max_y or (size[2] > 1 and (z == 0 or z == max_z)):
         return True
 
     # On the border of object:
     # offsets to the neighbors [[dx,dy,dz],[dx,dy,dz],...]; dx stands for "delta in x"
-    neigs = [[0,0,-1], [0,-1,0], [-1,0,0], [1,0,0], [0,1,0], [0,0,1]]
-    for dx,dy,dz in neigs:
-        if image[z+dz][y+dy][x+dx] != val:
+    neighs = [[0, -1, 0], [-1, 0, 0], [1, 0, 0], [0, 1, 0]]
+    if size[2] > 1:
+        neighs += [[0, 0, -1], [0, 0, 1]]
+    for dx, dy, dz in neighs:
+        if image[z + dz][y + dy][x + dx] != val:
             return True
     return False
 
